5dedee191f157c1def546be69baafdd6d9743962
Ruben Barros <atosc08@gmail.com>
2021-11-10 02:30:55 +0100

    Resolve Instance ?

--- deps.ts
+++ deps.ts
@@ -10,3 +10,5 @@ export { Command, CommandClient, Embed, GatewayIntents };
 export type { CommandContext };
 
 export { config } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
+
+export { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
---
+++ mod.ts
@@ -0,0 +1,10 @@
+import { Application } from "./deps.ts"
+import "./bot.ts"
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
